f39e6f08510458760bd75aff5f555fc29446912f
David Pérez <deividpa022@gmail.com>
2025-02-17 23:38:21 -0500

    feat: Add additional routes for about and countries pages

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"), 
+  route("about", "routes/about.tsx"),
+  route("countries", "routes/countries.tsx"),
+  route("countries/:country", "routes/country.tsx"),
+] satisfies RouteConfig;

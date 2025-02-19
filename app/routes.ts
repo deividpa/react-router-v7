901144e257b14ccc10bf7cd4b6566641bfac90c4
David Pérez <deividpa022@gmail.com>
2025-02-18 21:05:02 -0500

    feat: Refactor routing for countries to use prefix for cleaner structure

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,10 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"), 
   route("about", "routes/about.tsx"),
-  route("countries", "routes/countries.tsx"),
-  route("countries/:country", "routes/country.tsx"),
+  ...prefix("countries", [
+    route("", "routes/countries.tsx"),
+    route(":country", "routes/country.tsx"),
+  ]),
 ] satisfies RouteConfig;

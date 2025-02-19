0833509be1fc897b462b9d98b1aa6e30b1b4a98b
David Pérez <deividpa022@gmail.com>
2025-02-18 21:32:21 -0500

    feat: Implement clientLoader for fetching country data from API

--- app/routes/countries.tsx
+++ app/routes/countries.tsx
@@ -1,3 +1,13 @@
-export default function Countries() {
+import type { Route } from "./+types/countries";
+
+export async function clientLoader() {
+  // fetch data from https://restcountries.com/v3.1/all
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const data = await res.json();
+  return { data };
+}
+
+export default function Countries({ loaderData }: Route.ComponentProps) {
+  console.log(loaderData)
   return <div>Countries Page</div>;
 }

35ab99f14f244092779b71647aea9eb814fe83e2
David Pérez <deividpa022@gmail.com>
2025-02-18 21:04:56 -0500

    feat: Add NavBar and Footer components to App layout

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import NavBar from "./components/NavBar";
+import { Footer } from "./components/footer";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -42,7 +44,16 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+      <div className="flex-grow flex">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  )
+  
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {

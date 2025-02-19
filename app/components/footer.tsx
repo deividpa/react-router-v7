a991c6e44165d2b158c13851f409bf246b545d65
David Pérez <deividpa022@gmail.com>
2025-02-18 21:05:06 -0500

    feat: Add Footer component with social links and copyright notice

---
+++ app/components/footer.tsx
@@ -0,0 +1,26 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { Link } from "react-router";
+
+export function Footer() {
+  return (
+    <footer className="shadow-lg text-white py-6">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} RESTExplorer: Made with ❤️ by David Pérez
+        </p>
+        <div className="flex space-x-4 mt-4 md:mt-0">
+          <Link
+            to="https://github.com/deividpa"
+          >
+            <FaGithub className="w-6 h-6 hover:text-gray-300 transition" />
+          </Link>
+          <Link
+            to="https://www.linkedin.com/in/david-perez-aguirre/"
+          >
+            <FaLinkedin className="w-6 h-6 hover:text-gray-300 transition" />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
